375d19a2d6dbd66ddfe8fc525241e878cbcbe7d3
Finesse Carter <shop.scrxvms@gmail.com>
2020-04-13 22:49:26 -0400

    pic uploads

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,24 @@
-module.exports = function(app, passport, db) {
+module.exports = function(app, passport, db, multer, ObjectId) {
+
+// Image Upload Code =========================================================================
+// Make a Var for the storing of imgs => multer.(multer Method?)
+var storage = multer.diskStorage({
+
+  destination: (req, file, cb) => {    //What is cb? ... Maybe filepath
+    cb(null, 'public/images/uploads')
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now() + ".png")  // cb filepath and timestamp with date and filetype
+  }
+});
+var upload = multer({storage: storage}); //upload img to destination 'storage'
 
 // normal routes ===============================================================
 
     // show the home page (will also have our login links)
     app.get('/', function(req, res) {
         res.render('index.ejs');
-    }); 
+    });
       app.get('/gallery', function(req, res) {
       res.render('gallery.ejs');
     });
@@ -26,19 +39,65 @@ module.exports = function(app, passport, db) {
     });
     // PROFILE SECTION =========================
     app.get('/profile', isLoggedIn, function(req, res) {
+      console.log(req.user.local.email)
+      let uId = ObjectId(req.session.passport.user)   //uId = unique id from passport
       if(req.user.local.email){
-        //console.log(req.user.local)
         db.collection('messages').find().toArray((err, result) => {
+
+          db.collection('posts').find({'posterId': uId}).toArray((err, result) => {
           //console.log(result)
-          console.log(req)
+          // console.log(req)
           if (err) return console.log(err)
           res.render('profile.ejs', {
             user: req.user,
-            messages: result
+            messages: result,
+            posts: result   //post = result from DB
           })
         })
-      }
+      })
+    }
     });
+
+
+// FEED PAGE =========================
+app.get('/feed', function(req, res) {
+  db.collection('posts').find().toArray((err, result) => {  //Find all posts then turn to array
+    if (err) return console.log(err)
+    res.render('feed.ejs', {   //render /feed
+      user : req.user,
+      posts: result
+    })
+  })
+});
+
+// INDIVIDUAL POST PAGE =========================
+app.get('/post/:zebra', function(req, res) {  //  /:zebra = query param
+  let postId = ObjectId(req.params.zebra)   // postId = the queryParam unique number
+  console.log(postId);
+  db.collection('posts').find({_id: postId}).toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('post.ejs', {
+      posts: result
+    })
+  })
+});
+
+//Create Post =========================================================================
+app.post('/qpPost', upload.single('file-to-upload'), (req, res, next) => {  //one picture to post   //next????
+let uId = ObjectId(req.session.passport.user) // uId === the individual
+db.collection('posts').save({
+  posterId: uId,
+  caption: req.body.caption,
+  likes: 0,
+  imgPath: 'images/uploads/' + req.file.filename
+},
+(err, result) => {
+  if (err) return console.log(err)
+  console.log('saved to database')
+  res.redirect('/profile')
+})
+});
+
 
   //   app.get(`/profile${user.local.email}`, isLoggedIn, function(req, res) {
   //     db.collection('messages').find().toArray((err, result) => {
@@ -61,7 +120,7 @@ module.exports = function(app, passport, db) {
 app.post('/messages', (req, res) => {
   db.collection('messages').save({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
     if (err) return console.log(err)
-    console.log('saved to database')
+    console.log('MESSAGES SAVED TO DATABAS')
     res.redirect('/profile')
   })
 })
@@ -69,7 +128,7 @@ app.put('/messages', (req, res) => {
   db.collection('messages')
   .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
     $set: {
-      
+
       thumbUp:req.body.thumbUp + 1
       //thumbDown:req.body.thumbDown - 1
     }
@@ -88,7 +147,7 @@ app.put('/thumbdown', (req, res) => {
     $set: {
       thumbUp:req.body.thumbUp - 1
       //thumbDown:req.body.thumbDown - 1
-      
+
     }
   }, {
     sort: {_id: -1},
@@ -104,6 +163,37 @@ app.delete('/messages', (req, res) => {
     res.send('Message deleted!')
   })
 })
+// app.delete('/deltePictures', (req, res) => {
+//   db.collection('posts').findOneAndDelete({posterId: uId}, (err, result) => {
+//     if (err) return res.send(500, err)
+//     res.send('Photo deleted!')
+//   })
+// })
+
+
+app.delete('/posts', isLoggedIn, (req, res) => {
+  console.log(req.body)
+  const curUser= req.body.id
+  let uId = ObjectId(req.session.passport.user)
+  console.log(uId)
+  db.collection('posts').findOneAndDelete({
+    // caption: req.body.caption,
+    // likes: req.body.likes,
+    // imgPath: req.body.imgPath,
+    _id: ObjectId(req.body._id),
+    // posterId: uId
+  }, (err, result) => {
+    if (err) return res.send(500, err)
+    // if(result.value === null){
+    //   res.send(404, "not found mike pennisi")
+    //   return
+    // }
+    res.send('Message deleted!')
+  })
+})
+
+
+
 
 
 // =============================================================================
--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
 var thumbUp = document.getElementsByClassName("fa-thumbs-up");
 var trash = document.getElementsByClassName("fa-trash");
 var thumbDown = document.getElementsByClassName("fa-thumbs-down")
+var deletePic = document.getElementsByClassName('deletePic')
 Array.from(thumbUp).forEach(function(element) {
       element.addEventListener('click', function(){
         const name = this.parentNode.parentNode.childNodes[1].innerText
@@ -65,3 +66,68 @@ Array.from(trash).forEach(function(element) {
         })
       });
 });
+
+
+
+Array.from(deletePic).forEach(function(element) {
+      element.addEventListener('click', function(){
+        console.log('THIS DELETE BUTTON WORKS')
+        const caption = this.parentNode.childNodes[1].innerText
+        console.log(caption)
+        const info = this.parentNode.childNodes
+        console.log(info)
+        const likes = this.parentNode.childNodes[5].innerText
+        console.log(likes)
+        const imgPath = this.parentNode.childNodes[3].pathname
+        const deleteButton = this.parentNode.childNodes[9]
+        const _id = this.parentNode.childNodes[12].previousSibling.dataset.id
+        console.log(_id)
+        console.log(imgPath + ' imgPath')
+        fetch('posts', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'imgPath': imgPath,
+            'caption': caption,
+            'likes': Number(likes),
+            '_id': _id,
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
+
+// deletePic.addEventListener('click', function(){
+  // console.log('DELETE BUTTON')
+  // const imgPath = this.parentNode.parentNode.childNodes[3].src
+  // // const _id = this.parentNode.parentNode.getAttribute("data-id")
+  //  console.log(this.parentNode.parentNode)
+  // const caption = this.parentNode.parentNode.childNodes[5].innerText
+  // const likes = this.parentNode.parentNode.childNodes[7].innerText //
+//    fetch('deltePictures', {
+//     method: 'delete',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//       'imgPath': imgPath,
+//     })
+//   }).then(function (response) {
+//     if(response.ok === false){
+//       alert("delete failed")
+//       return
+//     }else{
+//     const postElement = element.closest("li")
+//     postElement.remove()
+//   //  window.location.reload()
+//     }
+//   })
+//   //maybe?
+//   .then(data => {
+//     console.log(data)
+//     window.location.reload(true)
+//   })
+// });
